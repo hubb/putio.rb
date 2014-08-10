6288ed83ef9b44c11d07fcf38088b7ec9803191d
Thibault Gautriaud <hubbbbb@gmail.com>
2014-08-10 15:48:01 +0100

    Starts with a fresh Putio when we need it

--- spec/lib/putio_spec.rb
+++ spec/lib/putio_spec.rb
@@ -5,18 +5,18 @@ describe Putio do
 
   describe 'client' do
     let(:access_token) { 'foobar' }
-    before do
-      Putio.reset!
-      Putio.configure { access_token = access_token }
-    end
 
     it { expect(Putio.client).to be_kind_of(Putio::Client) }
 
     it 'should memoize the client' do
+      Putio.reset!
+      Putio.configure { access_token = access_token }
       expect(Putio.client).to equal(Putio.client)
     end
 
     it 'should have a client with the same options' do
+      Putio.reset!
+      Putio.configure { access_token = access_token }
       expect(Putio.client.options).to eq(Putio.options)
     end
   end
